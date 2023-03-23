da8ce4188e6c91513f06d491b6fc1f6efa07e1c5
Andrés Cantillo <andrescanve@gmail.com>
2023-03-22 22:05:10 -0500

    added part1 1.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -16,6 +16,17 @@ export const App = () => {
     value: 0,
   });
 
+  const [selected, setSelected] = useState(0);
+
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+  ];
+
   const handleGood = () => {
     const { value } = good;
     setGood({ ...good, value: value + 1 });
@@ -29,6 +40,12 @@ export const App = () => {
   const handleNeutral = () => {
     const { value } = neutral;
     setNeutral({ ...neutral, value: value + 1 });
+  };
+
+  const handleSelected = () => {
+    //set a random value
+    const index = Math.floor(Math.random() * anecdotes.length);
+    setSelected(index);
   };
 
   return (
@@ -61,6 +78,11 @@ export const App = () => {
           </p>
         </div>
       )}
+      <div>
+        <h2>Anecdotes</h2>
+        <p>{anecdotes[selected]}</p>
+        <button onClick={handleSelected}>Random quote</button>
+      </div>
     </div>
   );
 };
