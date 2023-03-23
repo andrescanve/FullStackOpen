3fe0c32c2319028acaea79be9c9470a592a224c9
Andrés Cantillo <andrescanve@gmail.com>
2023-03-22 21:34:28 -0500

    added part1 1.7

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,34 +1,54 @@
 import { useState } from "react";
 import { Button } from "./components/Button";
+import { Statistic } from "./components/Statistic";
 
 export const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [good, setGood] = useState({
+    text: "Good",
+    value: 0,
+  });
+  const [neutral, setNeutral] = useState({
+    text: "Neutral",
+    value: 0,
+  });
+  const [bad, setBad] = useState({
+    text: "Bad",
+    value: 0,
+  });
 
   const handleGood = () => {
-    setGood(good + 1);
+    const { value } = good;
+    setGood({ ...good, value: value + 1 });
   };
 
   const handleBad = () => {
-    setBad(bad + 1);
+    const { value } = bad;
+    setBad({ ...bad, value: value + 1 });
   };
 
   const handleNeutral = () => {
-    setNeutral(neutral + 1);
+    const { value } = neutral;
+    setNeutral({ ...neutral, value: value + 1 });
   };
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={handleGood} text="Good" />
-      <Button handleClick={handleNeutral} text="Neutral" />
-      <Button handleClick={handleBad} text="Bad" />
+      <Button handleClick={handleGood} text={good.text} />
+      <Button handleClick={handleNeutral} text={neutral.text} />
+      <Button handleClick={handleBad} text={bad.text} />
 
-      <h2>Statics</h2>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <h2>Statistic</h2>
+      <Statistic text={good.text} value={good.value} />
+      <Statistic text={neutral.text} value={neutral.value} />
+      <Statistic text={bad.text} value={bad.value} />
+
+      <p>all: {good.value + neutral.value + bad.value}</p>
+      <p>average: {(good.value + neutral.value + bad.value) / 3}</p>
+      <p>
+        positive:{" "}
+        {(good.value * 100) / (good.value + neutral.value + bad.value)}
+      </p>
     </div>
   );
 };
