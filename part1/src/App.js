acda607042e499547b3654ff8dc6082552c1f7a6
Andrés Cantillo <andrescanve@gmail.com>
2023-03-24 05:14:55 -0500

    added part1 1.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -18,14 +18,29 @@ export const App = () => {
 
   const [selected, setSelected] = useState(0);
 
-  const anecdotes = [
-    "If it hurts, do it more often",
-    "Adding manpower to a late software project makes it later!",
-    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-    "Premature optimization is the root of all evil.",
-    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-  ];
+  const [anecdotes, setAnecdotes] = useState([
+    { quote: "If it hurts, do it more often", votes: 0 },
+    {
+      quote: "Adding manpower to a late software project makes it later!",
+      votes: 0,
+    },
+    {
+      quote:
+        "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+      votes: 0,
+    },
+    {
+      quote:
+        "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+      votes: 0,
+    },
+    { quote: "Premature optimization is the root of all evil.", votes: 0 },
+    {
+      quote:
+        "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+      votes: 0,
+    },
+  ]);
 
   const handleGood = () => {
     const { value } = good;
@@ -46,6 +61,13 @@ export const App = () => {
     //set a random value
     const index = Math.floor(Math.random() * anecdotes.length);
     setSelected(index);
+  };
+
+  const handleVote = () => {
+    const quote = anecdotes[selected];
+    quote.votes += 1;
+
+    setAnecdotes([...anecdotes, quote]);
   };
 
   return (
@@ -80,8 +102,10 @@ export const App = () => {
       )}
       <div>
         <h2>Anecdotes</h2>
-        <p>{anecdotes[selected]}</p>
+        <p>{anecdotes[selected].quote}</p>
+        <p>Votes: {anecdotes[selected].votes}</p>
         <button onClick={handleSelected}>Random quote</button>
+        <button onClick={handleVote}>vote</button>
       </div>
     </div>
   );
