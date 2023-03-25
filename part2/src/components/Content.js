16cc1267d8192ba6aac6c4db74f819ab6d939f7b
Andrés Cantillo <andrescanve@gmail.com>
2023-03-25 06:54:21 -0500

    added part2 2.2

--- part2/src/components/Content.js
+++ part2/src/components/Content.js
@@ -2,11 +2,18 @@ import React from "react";
 import { Part } from "./Part";
 
 export const Content = ({ parts }) => {
+  const getTotal = () => {
+    const exercises = parts.map((part) => part.exercises);
+    const total = exercises.reduce((total, exercise) => total + exercise, 0);
+
+    return total;
+  };
   return (
     <div>
       {parts.map((part) => (
         <Part key={part.id} part={part} />
       ))}
+      <p>Total of {getTotal()} exercises</p>
     </div>
   );
 };
